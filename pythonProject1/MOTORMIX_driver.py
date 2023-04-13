7da48c2ca40062180a6b68c29634bb8c6436c423
Andi <you@example.com>
2023-04-13 18:40:30 +0200

    code cleanup und bugfix buttons

--- pythonProject1/MOTORMIX_driver.py
+++ pythonProject1/MOTORMIX_driver.py
@@ -3,7 +3,7 @@ import mido
 FADER_VALUES = [7]
 GENERAL_BUFFER = []
 inport = ""
-outport = mido.open_output('USB Midi Cable 1')
+outport = mido.open_output('USB MIDI Interface 1')
 
 LEFT_BUTTON_FLAG = False
 RIGHT_BUTTON_FLAG = False
@@ -50,123 +50,94 @@ def to_msb_lsb(num):
 
     return msb_hex, lsb_hex
 
-with mido.open_input('USB Midi Cable 0') as inport:
+
+with mido.open_input('USB MIDI Interface 0') as inport:
     for message in inport:
         # Convert the incoming message to a hex string and strip any whitespace or '0x' prefix
         hex_message = ''.join(format(byte, '02X') for byte in message.bytes())
+
+
         # -------LEFT BUTTON BLOCK-------
         if hex_message == 'B00F08':
             LEFT_BUTTON_FLAG = True
-
-        elif hex_message == 'B02F40':
-            if LEFT_BUTTON_FLAG:
+        
+        elif LEFT_BUTTON_FLAG:
+            if hex_message == 'B02F40':
                 print("SHIFT pressed")
-                LEFT_BUTTON_FLAG = False
-
-        elif hex_message == 'B02F00':
-            if LEFT_BUTTON_FLAG:
-                print("SHIFT depressed")
-                GENERAL_BUFFER.clear()
-                LEFT_BUTTON_FLAG = False
-
-        elif hex_message == 'B02F41':
-            if LEFT_BUTTON_FLAG:
+            
+            elif hex_message == 'B02F41':
                 print("UNDO pressed")
-                LEFT_BUTTON_FLAG = False
-
-        elif hex_message == 'B02F42':
-            if LEFT_BUTTON_FLAG:
-                print("DEFAULT pressed")
-                LEFT_BUTTON_FLAG = False
-
-        elif hex_message == 'B02F43':
-            if LEFT_BUTTON_FLAG:
+            
+            elif hex_message == 'B02F42':
+                print("DEF pressed")
+            
+            elif hex_message == 'B02F43':
                 print("ALL pressed")
-                LEFT_BUTTON_FLAG = False
+            
+            elif hex_message == 'B02F44':
+                print("WIND pressed")
 
-        elif hex_message == 'B02F44':
-            if LEFT_BUTTON_FLAG:
-                print("WINDOW pressed")
-                LEFT_BUTTON_FLAG = False
+            elif hex_message == 'B02F45':
+                print("PLUG pressed")
 
-        elif hex_message == 'B02F45':
-            if LEFT_BUTTON_FLAG:
-                print("PLUGIN pressed")
-                LEFT_BUTTON_FLAG = False
+            elif hex_message == 'B02F46':
+                print("SUS pressed")
+            
+            elif hex_message == 'B02F47':
+                print("ENBL pressed")
+            
+            LEFT_BUTTON_FLAG = False
 
-        elif hex_message == 'B02F46':
-            if LEFT_BUTTON_FLAG:
-                print("SUSPEND pressed")
-                LEFT_BUTTON_FLAG = False
-
-        elif hex_message == 'B02F47':
-            if LEFT_BUTTON_FLAG:
-                print("AUTOENBL pressed")
-                LEFT_BUTTON_FLAG = False
-        LEFT_BUTTON_FLAG = False
 
         # -------RIGHT BUTTON BLOCK-------
         if hex_message == 'B00F09':
             RIGHT_BUTTON_FLAG = True
-            continue
-
-        elif hex_message == 'B02F40':
-            if RIGHT_BUTTON_FLAG:
-                print("ESCAPE pressed")
-                RIGHT_BUTTON_FLAG = False
-
-        elif hex_message == 'B02F41':
-            if RIGHT_BUTTON_FLAG:
+        
+        elif RIGHT_BUTTON_FLAG:
+            if hex_message == 'B02F40':
+                print("ESC pressed")
+            
+            elif hex_message == 'B02F41':
                 print("ENTER pressed")
-                RIGHT_BUTTON_FLAG = False
-
-        elif hex_message == 'B02F42':
-            if RIGHT_BUTTON_FLAG:
+            
+            elif hex_message == 'B02F42':
                 print("LAST pressed")
-                RIGHT_BUTTON_FLAG = False
-
-        elif hex_message == 'B02F43':
-            if RIGHT_BUTTON_FLAG:
+            
+            elif hex_message == 'B02F43':
                 print("NEXT pressed")
-                RIGHT_BUTTON_FLAG = False
-
-        elif hex_message == 'B02F44':
-            if RIGHT_BUTTON_FLAG:
+            
+            elif hex_message == 'B02F44':
                 print("REWIND pressed")
-                RIGHT_BUTTON_FLAG = False
 
-        elif hex_message == 'B02F45':
-            if RIGHT_BUTTON_FLAG:
-                print("F FWD pressed")
-                RIGHT_BUTTON_FLAG = False
+            elif hex_message == 'B02F45':
+                print("FWD pressed")
 
-        elif hex_message == 'B02F46':
-            if RIGHT_BUTTON_FLAG:
+            elif hex_message == 'B02F46':
                 print("STOP pressed")
-                RIGHT_BUTTON_FLAG = False
-
-        elif hex_message == 'B02F47':
-            if RIGHT_BUTTON_FLAG:
+            
+            elif hex_message == 'B02F47':
                 print("PLAY pressed")
-                RIGHT_BUTTON_FLAG = False
-        RIGHT_BUTTON_FLAG = False
+            
+            RIGHT_BUTTON_FLAG = False
+
+        
 
         # -----FADER 1-----
         if hex_message == 'B00F00':
             FADER_TOUCH_FLAG_1 = True
-            continue
+        
         elif (hex_message == 'B02F40') & FADER_TOUCH_FLAG_1:
             FADER_TOUCH_1 = True
             FADER_TOUCH_FLAG_1 = False
             print("FADER 1 ACTIVE")
-            continue
+        
         elif (hex_message == 'B02F00') & FADER_TOUCH_FLAG_1:
             FADER_TOUCH_1 = False
             FADER_TOUCH_FLAG_1 = False
             print("FADER 1 INACTIVE")
-            continue
 
         if (FADER_TOUCH_1):
+            GENERAL_BUFFER.clear
             GENERAL_BUFFER.append(hex_message)
             if (len(GENERAL_BUFFER) > 1):
                 position = parse_fader_position(GENERAL_BUFFER[0], GENERAL_BUFFER[1])

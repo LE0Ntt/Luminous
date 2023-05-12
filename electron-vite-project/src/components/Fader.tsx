df1b951a01305aa0be4c3b5c1789b992beeb260f
Leon Hölzel <leon-hoelzel@gmx.de>
2023-05-12 02:07:16 +0200

    fader quasi fertig

--- electron-vite-project/src/components/Fader.tsx
+++ electron-vite-project/src/components/Fader.tsx
@@ -1,10 +1,10 @@
 /**
  * Fader.tsx
  * heißt nur VolumeSlider, weil ich das mal so gemacht habe und es jetzt zu viel Aufwand wäre, das zu ändern
- * @author Leon Hölzel
+ * @author Leon Hölzel, Darwin Pietas
  */
 import React, { useState, ChangeEvent } from "react";
-import "./VolumeSlider.css";
+import "./Fader.css";
 
 interface VolumeSliderProps {
   initialVolume?: number;
@@ -26,8 +26,8 @@ const Fader: React.FC<VolumeSliderProps> = ({
   };
 
   return (
-    <div>
-      <div className="volume-slider h-[300px]">
+    <div className="h-[340px] w-[82px] bg-purple-400 p-4 m-4 relative">
+      <div className="volume-slider">
         <input
           type="range"
           min="0"
@@ -35,10 +35,20 @@ const Fader: React.FC<VolumeSliderProps> = ({
           step="1"
           value={volume}
           onChange={handleVolumeChange}
+          style={{ background: `linear-gradient(to right, #4F53B1 0%, #4F53B1 ${volume}%, rgba(40, 40, 40, 0.7) ${volume}%, rgba(40, 40, 40, 0.7) 100%)`}}
           className="slider"
         />
-        <span>{volume}%</span>
       </div>
+      <div>
+        <input
+        type="number"
+        value={volume}
+        onChange={handleVolumeChange}
+        className="inputNum w-12 h-8 text-right bg-transparent"
+        />
+        <div className="absolute right-[28px] bottom-[30px]">%</div>
+      </div>
+      
     </div>
   );
 };

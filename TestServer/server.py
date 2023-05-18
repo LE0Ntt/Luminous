0e142926f0135594b8dec0fde451c4a12e56dd02
Darwin <dnine@outlook.de>
2023-05-19 01:49:28 +0200

    scrollleiste

--- TestServer/server.py
+++ TestServer/server.py
@@ -21,14 +21,14 @@ def handle_volume(new_volume):
     volume = new_volume
     print('Received volume:', volume)
     # Do something with the volume...
-
+'''
 @socketio.on('volume')
 def sendvariable():
     while True:
         socketio.emit('volume', volume, broadcast=True)
         print('Sent volume:', volume)
         time.sleep(1)
-
+'''
 
 if __name__ == '__main__':
     socketio.run(app, host='127.0.0.1', port=5000)

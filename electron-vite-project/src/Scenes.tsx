eb3fe257ae9b75dfc02f916d23ec93c3ee19787f
Darwin <dnine@outlook.de>
2023-05-19 00:00:58 +0200

    fader nebeneinander

--- electron-vite-project/src/Scenes.tsx
+++ electron-vite-project/src/Scenes.tsx
@@ -2,17 +2,21 @@
  * Scenes.tsx
  * @author Leon Hölzel
  */
-import React from 'react'
+import React, { useContext } from 'react'
 import './App.css'
+import { TranslationContext } from "./TranslationContext";
 
 function Scenes() {
+  const { t } = useContext(TranslationContext);
+
   return (
     <div>
       <div className='window absolute w-[200px] h-[910px] left-[40px] top-[130px]'>
+        <h1>{t("greeting")}</h1>
       </div>
       <div className='window absolute w-[1620px] h-[910px] left-[260px] top-[130px]'></div>
     </div>
-  )
+  );
 }
 
-export default Scenes
+export default Scenes;

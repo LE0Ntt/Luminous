a6582d2a4d5e0233107af98b27a1dea37dc67852
Darwin <dnine@outlook.de>
2023-05-18 05:52:39 +0200

    aufräumen + fix dragging fix + fader intern > 255

--- electron-vite-project/src/Studio.tsx
+++ electron-vite-project/src/Studio.tsx
@@ -7,24 +7,10 @@ import Button from './components/Button';
 import Fader from './components/Fader';
 import './index.css';
 import './Studio.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 const Studio = () => {
-
-  const [data, setData] = useState([{}]);
-/*
-  useEffect(() => { //test um direkt was Ã¼ber api abzurufen
-    fetch("http://localhost:5000/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  },  []);
-*/
   // Button test
   const handleClick = (id: number) => {
     console.log('Button clicked!' + id);
@@ -38,60 +24,25 @@ const Studio = () => {
 
   const [sliders, setSliders] = useState<SliderConfig[]>([{ id: 1, initialVolume: 0 }]);
 
-  const [socketInstance, setSocketInstance] = useState<Socket>();
+  //const [socketInstance, setSocketInstance] = useState<Socket>();
+  const socketInstance = useRef<Socket>();
   // Connect to the socket.io server on the backend
   useEffect(() => {
-    const socket = io("http://127.0.0.1:5000/test", {
-      transports: ["websocket"],
-      withCredentials: true
-    });
+    socketInstance.current = io(`http://${window.location.hostname}:5000/test`);
 
-    setSocketInstance(socket);
-
-    socket.on("variable_update", (data) => {
+    socketInstance.current?.on("variable_update", (data) => {
       console.log(data);
     });
-
-    return function cleanup() {
-      socket.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (socketInstance) 
-    socketInstance.on("variable_update", (data) => {
-      console.log(data);
-    });
-    // When the server sends an update for a slider value, update the corresponding slider
-    console.log("update")
-    if (socketInstance) 
-      socketInstance.on('variable_update', (updatedSlider: SliderConfig) => {
-        setSliders((prevSliders) =>
-          prevSliders.map((slider) =>
-            slider.id === updatedSlider.id ? updatedSlider : slider
-          )
-        );
-      });
-
-    return function cleanup() {
-      if (socketInstance) 
-      socketInstance.off("variable_update");
-    };
-  }, [socketInstance]);
-
-/*
-  // When the component mounts, get the initial slider data from the server
-  useEffect(() => {
-    socket.on('initial_sliders', (initialSliders: SliderConfig[]) => {
+    // When the component mounts, get the initial slider data from the server
+    socketInstance.current?.on('initial_sliders', (initialSliders: SliderConfig[]) => {
       setSliders(initialSliders);
     });
+    return () => { socketInstance.current?.disconnect(); };
   }, []);
-*/
+
   // When a slider value changes, emit an event to the server with the updated value
   const handleVolumeChange = (id: number, volume: number) => {
-    console.log({volume});
-    if (socketInstance) 
-      socketInstance.emit('slider_change', { id, volume });
+    socketInstance.current?.emit('fader_value', { value: volume });
   };
 
   // Add a new slider to the server and update the client with the new slider data
@@ -111,21 +62,25 @@ const Studio = () => {
       <div className='h-20 w-20'></div>
       <div>
         <h1>Volume Sliders</h1>
-      <button onClick={addSlider}>Add Slider</button>
-      <div className='faders window'>
-        <div className="sliders">
-          {sliders.map((slider) => (
-            <div key={slider.id}>
-              <h2>Slider {slider.id}</h2>
-              <Fader
-                initialVolume={slider.initialVolume}
-                onVolumeChange={(volume) => handleVolumeChange(slider.id, volume)}
-              />
-              <Button onClick={() => handleClick(slider.id)}>Button</Button>
-            </div>
-          ))}
+        <button onClick={addSlider}>Add Slider</button>
+        <div className='faders window'>
+        { socketInstance ? (
+          <div className="sliders">
+            {sliders.map((slider) => (
+              <div key={slider.id}>
+                <h2>Slider {slider.id}</h2>
+                <Fader
+                  initialVolume={slider.initialVolume}
+                  onVolumeChange={(volume) => handleVolumeChange(slider.id, volume)}
+                />
+                <Button onClick={() => handleClick(slider.id)}>Button</Button>
+              </div>
+            ))}
+          </div>
+          ) : (
+            <div>Not Connected</div>
+          )}
         </div>
-      </div>
       </div>
     </div>
   )

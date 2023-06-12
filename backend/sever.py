734e371d01482356976603c40bb65a8ff63c74b8
Darwin <dnine@outlook.de>
2023-06-12 04:28:16 +0200

    fix server, fix double messages on fader

--- backend/sever.py
+++ backend/sever.py
@@ -1,9 +1,11 @@
 from server import app, Driver, routes
 from flask import json
 from flask_socketio import SocketIO
+import socket_events
+from socket_events import socketio
+#socketio = SocketIO(app, cors_allowed_origins = "*")
 
-
-socketio = SocketIO(app, cors_allowed_origins = "*")
+socket_events.register_socketio_events(socketio)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
--- electron-vite-project/src/components/Fader.tsx
+++ electron-vite-project/src/components/Fader.tsx
@@ -17,47 +17,46 @@ const Fader: React.FC<SliderProps> = ({
 }) => {
   const { emit } = useConnectionContext();
   const [value, setValue] = useState<number>(sliderValue);
-  const faderClassName = height ? "fader faderMaster" : "fader";
   const [isDragging, setIsDragging] = useState(false);
-  const displayValue = Math.round((value / 255) * 100);
   const [timerRunning, setTimerRunning] = useState<boolean | null>(null);
   const [cacheValue, setCacheValue] = useState<number | null>(null);
-  const [mainColor, setMainColor] = useState("#4F53B1");
-  const [isDark, setIsDark] = useState(document.body.classList.contains('dark'));
-
-  useEffect(() => {
-    // Dark mode event listener
-    const handleDarkModeChange = () => setIsDark(document.body.classList.contains('dark'));
-    document.body.addEventListener('class-change', handleDarkModeChange);
-    return () => document.body.removeEventListener('class-change', handleDarkModeChange);
-  }, []);
-
-  useEffect(() => {
-    isDark ? setMainColor("#B9BCFF") : setMainColor("#4F53B1");
-  }, [isDark]);
+  const cacheValueRef = useRef<number>(sliderValue);
+  const sendValueRef = useRef<number>(sliderValue);
+  const faderClassName = height ? "fader faderMaster" : "fader";
+  const displayValue = Math.round((value / 255) * 100);
 
   useEffect(() => {
     if(!isDragging)
       setValue(sliderValue);
   }, [sliderValue]);
   
+  // Set fader height by the passed parameter
+  useEffect(() => {
+    if(height)
+      document.documentElement.style.setProperty("--sliderHeight", `${height}px`);
+  }, [height]);
+
+  // Always send the last value
+  useEffect(() => {
+    if(!timerRunning && cacheValueRef.current != null && cacheValueRef.current != sendValueRef.current)
+      emit("fader_value", { id: id, value: value });
+  }, [timerRunning]);
+
   const handleSliderChange = (event: ChangeEvent<HTMLInputElement>) => {
     let newValue = Math.min(Math.max(parseInt(event.target.value, 10), 0), 255);
     setValue(newValue);
-    setCacheValue(newValue);
+    cacheValueRef.current = newValue;
+
+    // Send only at certain time intervals 
     if(!timerRunning) {
       setTimerRunning(true);
       emit("fader_value", { id: id, value: newValue });
+      sendValueRef.current = newValue;
       setTimeout(() => {
         setTimerRunning(false);
       }, 20); // Timeout in ms
     }
   };
-
-  useEffect(() => {
-    if(!timerRunning && cacheValue != null)
-    emit("fader_value", { id: id, value: cacheValue });
-  }, [timerRunning]);
 
   const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
     let newValue = Math.min(Math.max(parseInt(event.target.value, 10), 0), 100);
@@ -65,12 +64,6 @@ const Fader: React.FC<SliderProps> = ({
     setValue(newValue);
     emit("fader_value", { id: id, value: newValue });
   };
-
-  useEffect(() => {
-    // Setze den Wert der CSS-Variable basierend auf der übergebenen Höhe
-    if(height)
-      document.documentElement.style.setProperty("--sliderHeight", `${height}px`);
-  }, [height]);
 
   return (
     <div className={faderClassName}>
@@ -86,7 +79,7 @@ const Fader: React.FC<SliderProps> = ({
           onMouseDown={() => setIsDragging(true)}
           onMouseUp={() => setIsDragging(false)}
           style={{
-            background: `linear-gradient(to right, ${mainColor} 0%, ${mainColor} ${displayValue}%, rgba(40, 40, 40, 0.7) ${displayValue}%, rgba(40, 40, 40, 0.7) 100%)`,
+            background: `linear-gradient(to right, var(--mainColor) 0%, var(--mainColor) ${displayValue}%, rgba(40, 40, 40, 0.7) ${displayValue}%, rgba(40, 40, 40, 0.7) 100%)`,
           }}
           className="slider sliderTrackColor"
         />

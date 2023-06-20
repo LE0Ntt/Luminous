76090ef080c03390ff6428bb48ebeafe7ff23634
Leon Hölzel <leon-hoelzel@gmx.de>
2023-06-20 14:29:09 +0200

    fixed import error

--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,7 @@ from flask import json
 from flask_socketio import SocketIO
 import socket_events
 from socket_events import socketio
-from socket_events import setup
+# from socket_events import setup
 # socketio = SocketIO(app, cors_allowed_origins = "*")
 
 socket_events.register_socketio_events(socketio)

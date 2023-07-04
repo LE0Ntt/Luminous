e7e1d4ef3b834aec29b01a20a40dadd6fd837037
BombusBombus <plate.m@mx.de>
2023-07-04 10:33:16 +0200

    green power led is on while server is running

--- backend/server.py
+++ backend/server.py
@@ -7,20 +7,36 @@ import signal
 import sys
 import motorMix_handler
 # from socket_events import setup
-socketio = SocketIO(app, cors_allowed_origins = "*")
+import RPi.GPIO as GPIO
 
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+#--------------LED-----------------#
+LED_PIN = 24
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+def turn_on_led():
+    GPIO.output(LED_PIN, GPIO.HIGH)
+
+def turn_off_led():
+    GPIO.output(LED_PIN, GPIO.LOW)
 
 def signal_handler(sig, frame):
     print('Server stopped.')
+    turn_off_led()
+    GPIO.cleanup()
     sys.exit(0)
+    
+#-------------LED-END----------------#
 
 #motorMix_handler.motorMix_updateDevices()
 
 signal.signal(signal.SIGINT, signal_handler)
 
-
 socket_events.register_socketio_events(socketio)
 
 if __name__ == '__main__':
-    # setup()
-    socketio.run(app, host='127.0.0.1', port=5000)
+    # LED einschalten
+    turn_on_led()
+    socketio.run(app, host='192.168.0.251', port=5000)

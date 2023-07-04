6d0b620d589f0ae2e02d0620bf8e9a0c2dd3371d
BombusBombus <plate.m@gmx.de>
2023-07-04 10:37:01 +0200

    LED function disabled for further use on other systems

--- backend/server.py
+++ backend/server.py
@@ -7,11 +7,12 @@ import signal
 import sys
 import motorMix_handler
 # from socket_events import setup
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 #--------------LED-----------------#
+'''
 LED_PIN = 24
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
@@ -27,7 +28,7 @@ def signal_handler(sig, frame):
     turn_off_led()
     GPIO.cleanup()
     sys.exit(0)
-    
+    '''
 #-------------LED-END----------------#
 
 #motorMix_handler.motorMix_updateDevices()
@@ -38,5 +39,5 @@ socket_events.register_socketio_events(socketio)
 
 if __name__ == '__main__':
     # LED einschalten
-    turn_on_led()
+    #turn_on_led()
     socketio.run(app, host='192.168.0.251', port=5000)

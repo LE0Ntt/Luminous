f2cbf7df830cdd259621bde426a70e3f63b96b6a
Leon Hölzel <leon-hoelzel@gmx.de>
2023-07-05 13:55:21 +0200

    added BiColor Support, Master macht den Leider noch kaputt

--- electron-vite-project/src/components/ControlHandler.tsx
+++ electron-vite-project/src/components/ControlHandler.tsx
@@ -1,5 +1,75 @@
 function ControlHandler(selectedDevices: any, red: number, green: number, blue: number, kelvin: number, master: number, setFaderValue: Function, emit: Function) {
   const values = {red, green, blue, kelvin, master};
+
+  // https://tannerhelland.com/2012/09/18/convert-temperature-rgb-algorithm-code.html
+  function kelvinToRgb(temperature: any) {
+    let temperatureNorm = temperature / 100;
+    let red, green, blue;
+  
+    // Berechne Rot
+    if (temperatureNorm <= 66) {
+      red = 255;
+    } else {
+      red = temperatureNorm - 60;
+      red = 329.698727446 * Math.pow(red, -0.1332047592);
+      if (red < 0) red = 0;
+      if (red > 255) red = 255;
+    }
+  
+    // Berechne Grün
+    if (temperatureNorm <= 66) {
+      green = temperatureNorm;
+      green = 99.4708025861 * Math.log(green) - 161.1195681661;
+      if (green < 0) green = 0;
+      if (green > 255) green = 255;
+    } else {
+      green = temperatureNorm - 60;
+      green = 288.1221695283 * Math.pow(green, -0.0755148492);
+      if (green < 0) green = 0;
+      if (green > 255) green = 255;
+    }
+  
+    // Berechne Blau
+    if (temperatureNorm >= 66) {
+      blue = 255;
+    } else {
+      if (temperatureNorm <= 19) {
+        blue = 0;
+      } else {
+        blue = temperatureNorm - 10;
+        blue = 138.5177312231 * Math.log(blue) - 305.0447927307;
+        if (blue < 0) blue = 0;
+        if (blue > 255) blue = 255;
+      }
+    }
+  
+    return [Math.round(red), Math.round(green), Math.round(blue)];
+  }
+  function rgbToKelvin(r: number, g: number, b: number) {
+    let minTemp = 1000;
+    let maxTemp = 40000;
+    const eps = 0.4;
+    let temp;
+
+    while (maxTemp - minTemp > eps) {
+        temp = (maxTemp + minTemp) / 2;
+        const rgbApprox = kelvinToRgb(temp);
+
+        if ((rgbApprox[2] / rgbApprox[0]) >= (b / r)) {
+            maxTemp = temp;
+        } else {
+            minTemp = temp;
+        }
+    }
+    return Math.round(temp || 0);
+  }
+  
+
+  let tempInKelvin = rgbToKelvin(red, green, blue);
+  kelvin = (tempInKelvin - 2220) / 8648 * 255;
+  console.log("Kelvin: ",tempInKelvin);
+  console.log("Kelvin0-255: ",kelvin);
+
   console.log(selectedDevices)
   selectedDevices.forEach((device: any) => {
     device.attributes.channel.forEach((channel: any) => {
@@ -19,7 +89,7 @@ function ControlHandler(selectedDevices: any, red: number, green: number, blue: 
         case "BiColor":
           switch(channel.id.toString()) {
             case '0': value = values.master || 0; break;
-            case '1': value = values.red || 0; break; // muss noch durch kelvin ersetzt werden
+            case '1': value = values.kelvin || 0; break; // muss noch durch kelvin ersetzt werden
           }
           console.log('setFaderValue', device.id, channel.id, value)
           setFaderValue(device.id, channel.id, value);

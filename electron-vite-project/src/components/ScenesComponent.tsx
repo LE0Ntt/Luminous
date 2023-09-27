b7ef2547cb016503cd22e7ea2ad2fbee5c1215e1
Darwin <dnine@outlook.de>
2023-09-27 21:29:25 +0200

    Fix if fade duration is not changed

--- electron-vite-project/src/components/ScenesComponent.tsx
+++ electron-vite-project/src/components/ScenesComponent.tsx
@@ -125,11 +125,12 @@ const ScenesComponent: React.FC<ScenesComponentProps> = ({ sideId, setAddScene, 
   const toggleSceneStatus = (sceneId: number) => { 
     const layer = localStorage.getItem('layer')  === 'true';
     setScenes((prevScenes) => {
+      const fadeDuration = parseInt(sessionStorage.getItem('fadeDuration') || '0');
       // Update scenes with status false if layer is off
       if (!layer) {
         prevScenes.forEach((scene) => {
           if (scene.status) {
-            emit("scene_update", { id: scene.id, status: false, fadeTime: sessionStorage.getItem('fadeDuration') });
+            emit("scene_update", { id: scene.id, status: false, fadeTime: fadeDuration });
           }
         });
       }
@@ -137,7 +138,7 @@ const ScenesComponent: React.FC<ScenesComponentProps> = ({ sideId, setAddScene, 
       const updatedScenes = prevScenes.map((scene) => {
         if (scene.id === sceneId) {
           const newStatus = !scene.status;
-          emit("scene_update", { id: sceneId, status: newStatus, fadeTime: sessionStorage.getItem('fadeDuration') });
+          emit("scene_update", { id: sceneId, status: newStatus, fadeTime: fadeDuration });
           return { ...scene, status: newStatus };
         }
         return scene;

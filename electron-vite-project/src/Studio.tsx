689ea4695aa1b4863b990e38fb90e1fe653d50f6
Darwin <dnine@outlook.de>
2023-10-27 03:48:20 +0200

    Test Studio left-right order

--- electron-vite-project/src/Studio.tsx
+++ electron-vite-project/src/Studio.tsx
@@ -28,6 +28,7 @@ import lampImage from './assets/lamp.png'
 import schein from './assets/schein3.png'
 import schein2 from './assets/schein2.png'
 import fillLight from './assets/fillLight.png'
+import Toggle from './components/Toggle';
 
 const Studio = () => {
   const navigate = useNavigate();
@@ -129,10 +130,62 @@ const Studio = () => {
     setAddScene(false);
   };
 
+  const [reverseOrder, setReverseOrder] = useState(false);
+  const handleToggleChange = (status: boolean | ((prevState: boolean) => boolean)) => {
+    localStorage.setItem('reverseOrder', `${status}`);
+    setReverseOrder(!reverseOrder);
+  };
+
   return (
-    <div>
-      <div className='scenes window'>
-        <ScenesComponent sideId={0} setAddScene={setAddScene} />
+    <div className="studioLayout" style={{flexDirection: localStorage.getItem('reverseOrder') === 'true' ? 'row-reverse' : 'row'}}>
+      <div style={{position:'absolute',top:'-30px',left:'0'}}><Toggle onClick={handleToggleChange} enabled={localStorage.getItem('reverseOrder') === 'true'}/></div>
+      <div className="scenesAndFaders">
+        <div className='scenes window'>
+          <ScenesComponent sideId={0} setAddScene={setAddScene} />
+        </div>
+        { !bigView && (
+          <>
+            <div className='mainfader window'>
+              { sliders[0] && (
+                <Fader
+                  height={340}
+                  id={0}
+                  sliderGroupId={0}
+                  name="Master"
+                />
+              )}
+            </div>
+            <div>
+              <div className='faders window'>
+                <div className="sliders">
+                  { sliders.slice(1).map((slider) => (
+                    <div key={slider.id} className='slidersHeight'>
+                      <h2 className='faderText'>{slider.id}</h2>
+                      <Fader
+                        key={slider.id}
+                        id={0}
+                        sliderGroupId={slider.id}
+                        name={slider.name}
+                      />
+                      <Button 
+                        onClick={() => handleClick(slider.id)} 
+                        className="buttonOpenControl"
+                      >
+                        <svg className='centerIcon' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="m19,20c0,.28-.1.52-.29.71-.19.19-.43.29-.71.29s-.52-.1-.71-.29c-.19-.19-.29-.43-.29-.71v-4c0-.28.1-.52.29-.71s.43-.29.71-.29.52.1.71.29.29.43.29.71v4Zm-12,0c0,.28-.1.52-.29.71-.19.19-.43.29-.71.29s-.52-.1-.71-.29c-.19-.19-.29-.43-.29-.71v-8c0-.28.1-.52.29-.71.19-.19.43-.29.71-.29s.52.1.71.29c.19.19.29.43.29.71v8Zm14-8c0,.28-.1.52-.29.71-.19.19-.43.29-.71.29h-4c-.28,0-.52-.1-.71-.29s-.29-.43-.29-.71.1-.52.29-.71c.19-.19.43-.29.71-.29h1V4c0-.28.1-.52.29-.71s.43-.29.71-.29.52.1.71.29.29.43.29.71v7h1c.28,0,.52.1.71.29.19.19.29.43.29.71Zm-6,4c0,.28-.1.52-.29.71s-.43.29-.71.29h-1v3c0,.28-.1.52-.29.71-.19.19-.43.29-.71.29s-.52-.1-.71-.29c-.19-.19-.29-.43-.29-.71v-3h-1c-.28,0-.52-.1-.71-.29s-.29-.43-.29-.71.1-.52.29-.71.43-.29.71-.29h4c.28,0,.52.1.71.29s.29.43.29.71Zm-2-4c0,.28-.1.52-.29.71s-.43.29-.71.29-.52-.1-.71-.29c-.19-.19-.29-.43-.29-.71V4c0-.28.1-.52.29-.71.19-.19.43-.29.71-.29s.52.1.71.29.29.43.29.71v8Zm-4-4c0,.28-.1.52-.29.71s-.43.29-.71.29h-4c-.28,0-.52-.1-.71-.29s-.29-.43-.29-.71.1-.52.29-.71.43-.29.71-.29h1v-3c0-.28.1-.52.29-.71s.43-.29.71-.29.52.1.71.29.29.43.29.71v3h1c.28,0,.52.1.71.29s.29.43.29.71Z"/></svg>
+                      </Button>
+                    </div>
+                  ))}
+                </div>
+                <Button
+                  onClick={() => openBigView()}
+                  className="buttonBigView"
+                >
+                  <svg className='centerIcon' xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h240q17 0 28.5 11.5T480-800q0 17-11.5 28.5T440-760H200v560h560v-240q0-17 11.5-28.5T800-480q17 0 28.5 11.5T840-440v240q0 33-23.5 56.5T760-120H200Zm160-240q-11-11-11-28t11-28l344-344H600q-17 0-28.5-11.5T560-800q0-17 11.5-28.5T600-840h200q17 0 28.5 11.5T840-800v200q0 17-11.5 28.5T800-560q-17 0-28.5-11.5T760-600v-104L415-359q-11 11-27 11t-28-12Z"/></svg>
+                </Button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
       <div className='overview window'>
         <div className='studio_overview window'>
@@ -291,50 +344,6 @@ const Studio = () => {
           </div>
         </div>
       </div>
-      { !bigView && (
-        <>
-          <div className='mainfader window'>
-            { sliders[0] && (
-              <Fader
-                height={340}
-                id={0}
-                sliderGroupId={0}
-                name="Master"
-              />
-            )}
-          </div>
-          <div>
-            <div className='faders window'>
-              <div className="sliders">
-                { sliders.slice(1).map((slider) => (
-                  <div key={slider.id} className='slidersHeight'>
-                    <h2 className='faderText'>{slider.id}</h2>
-                    <Fader
-                      key={slider.id}
-                      id={0}
-                      sliderGroupId={slider.id}
-                      name={slider.name}
-                    />
-                    <Button 
-                      onClick={() => handleClick(slider.id)} 
-                      className="buttonOpenControl"
-                    >
-                      <svg className='centerIcon' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="m19,20c0,.28-.1.52-.29.71-.19.19-.43.29-.71.29s-.52-.1-.71-.29c-.19-.19-.29-.43-.29-.71v-4c0-.28.1-.52.29-.71s.43-.29.71-.29.52.1.71.29.29.43.29.71v4Zm-12,0c0,.28-.1.52-.29.71-.19.19-.43.29-.71.29s-.52-.1-.71-.29c-.19-.19-.29-.43-.29-.71v-8c0-.28.1-.52.29-.71.19-.19.43-.29.71-.29s.52.1.71.29c.19.19.29.43.29.71v8Zm14-8c0,.28-.1.52-.29.71-.19.19-.43.29-.71.29h-4c-.28,0-.52-.1-.71-.29s-.29-.43-.29-.71.1-.52.29-.71c.19-.19.43-.29.71-.29h1V4c0-.28.1-.52.29-.71s.43-.29.71-.29.52.1.71.29.29.43.29.71v7h1c.28,0,.52.1.71.29.19.19.29.43.29.71Zm-6,4c0,.28-.1.52-.29.71s-.43.29-.71.29h-1v3c0,.28-.1.52-.29.71-.19.19-.43.29-.71.29s-.52-.1-.71-.29c-.19-.19-.29-.43-.29-.71v-3h-1c-.28,0-.52-.1-.71-.29s-.29-.43-.29-.71.1-.52.29-.71.43-.29.71-.29h4c.28,0,.52.1.71.29s.29.43.29.71Zm-2-4c0,.28-.1.52-.29.71s-.43.29-.71.29-.52-.1-.71-.29c-.19-.19-.29-.43-.29-.71V4c0-.28.1-.52.29-.71.19-.19.43-.29.71-.29s.52.1.71.29.29.43.29.71v8Zm-4-4c0,.28-.1.52-.29.71s-.43.29-.71.29h-4c-.28,0-.52-.1-.71-.29s-.29-.43-.29-.71.1-.52.29-.71.43-.29.71-.29h1v-3c0-.28.1-.52.29-.71s.43-.29.71-.29.52.1.71.29.29.43.29.71v3h1c.28,0,.52.1.71.29s.29.43.29.71Z"/></svg>
-                    </Button>
-                  </div>
-                ))}
-              </div>
-              <Button
-                onClick={() => openBigView()}
-                className="buttonBigView"
-              >
-                <svg className='centerIcon' xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h240q17 0 28.5 11.5T480-800q0 17-11.5 28.5T440-760H200v560h560v-240q0-17 11.5-28.5T800-480q17 0 28.5 11.5T840-440v240q0 33-23.5 56.5T760-120H200Zm160-240q-11-11-11-28t11-28l344-344H600q-17 0-28.5-11.5T560-800q0-17 11.5-28.5T600-840h200q17 0 28.5 11.5T840-800v200q0 17-11.5 28.5T800-560q-17 0-28.5-11.5T760-600v-104L415-359q-11 11-27 11t-28-12Z"/></svg>
-              </Button>
-              
-            </div>
-          </div>
-        </>
-      )}
       {bigView && <BigView onClose={closeBigView} />}
       {addScene && <AddScene onClose={closeAddScene} />}
     </div>

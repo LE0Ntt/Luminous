8f74094df553bcfedf4fa780dd37422d9d46f05f
Darwin <dnine@outlook.de>
2023-11-02 07:03:02 +0100

    Custom loading screen for Electron app

--- electron-vite-project/electron/preload/index.ts
+++ electron-vite-project/electron/preload/index.ts
@@ -25,48 +25,50 @@ const safeDOM = {
   },
 }
 
-/**
- * https://tobiasahlin.com/spinkit
- * https://connoratherton.com/loaders
- * https://projects.lukehaas.me/css-loaders
- * https://matejkustec.github.io/SpinThatShit
- */
 function useLoading() {
-  const className = `loaders-css__square-spin`
+  const className = `loaders-css`
   const styleContent = `
-@keyframes square-spin {
-  25% { transform: perspective(100px) rotateX(180deg) rotateY(0); }
-  50% { transform: perspective(100px) rotateX(180deg) rotateY(180deg); }
-  75% { transform: perspective(100px) rotateX(0) rotateY(180deg); }
-  100% { transform: perspective(100px) rotateX(0) rotateY(0); }
-}
-.${className} > div {
-  animation-fill-mode: both;
-  width: 50px;
-  height: 50px;
-  background: #fff;
-  animation: square-spin 3s 0s cubic-bezier(0.09, 0.57, 0.49, 0.9) infinite;
-}
-.app-loading-wrap {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #282c34;
-  z-index: 9;
-}
-    `
+    .${className} > div {
+      width: 256px;
+      height: 256px;
+      background: url('../src/assets/iconStartup.png') no-repeat center/cover;
+      opacity: .6;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .${className} > div > div {
+      margin-bottom: 80px;
+      width: 94px;
+      height: 94px;
+      background: url('../src/assets/iconReflection.png') no-repeat center/cover;
+      animation: spin .8s infinite linear;
+    }
+    .app-loading-wrap {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: url('../src/assets/bgLight.png') no-repeat center/cover;
+      z-index: 9;
+    }
+    @keyframes spin {
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  `
   const oStyle = document.createElement('style')
   const oDiv = document.createElement('div')
 
   oStyle.id = 'app-loading-style'
   oStyle.innerHTML = styleContent
   oDiv.className = 'app-loading-wrap'
-  oDiv.innerHTML = `<div class="${className}"><div></div></div>`
+  oDiv.innerHTML = `<div class="${className}"><div><div></div></div></div>`
 
   return {
     appendLoading() {

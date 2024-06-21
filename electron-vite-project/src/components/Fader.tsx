bca67b35312a3a4bfb46ea87c5f25c64be8b3a94
Darwin <dnine@outlook.de>
2024-06-21 14:55:23 +0200

    Fader revert comments back

--- electron-vite-project/src/components/Fader.tsx
+++ electron-vite-project/src/components/Fader.tsx
@@ -28,6 +28,7 @@ interface SliderProps {
 }
 
 const Fader: React.FC<SliderProps> = React.memo(({ id, sliderGroupId, name, number, height, className, color }) => {
+  // Initialize context
   const { emit } = useConnectionContext();
   const { setFaderValue } = useFaderContext();
   const faderValue = useFaderValue(sliderGroupId, id);
@@ -36,17 +37,21 @@ const Fader: React.FC<SliderProps> = React.memo(({ id, sliderGroupId, name, numb
   const [isHovered, setIsHovered] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
 
+  // Refs to keep track of fader values and if they need to be sent
   const cacheValueRef = useRef<number>(faderValue);
   const sendValueRef = useRef<number>(faderValue);
 
+  // Calculating the display value (0 to 100%) and update input value
   const scaledDisplayValue = (faderValue / 255) * 100;
   const [inputValue, setInputValue] = useState<any>(Math.round(scaledDisplayValue) + '%');
 
+  // Update input value when display value changes
   useEffect(() => {
     const finalDisplayValue = isFocused ? scaledDisplayValue.toFixed(1) : Math.round(scaledDisplayValue) + '%';
     setInputValue(finalDisplayValue);
   }, [scaledDisplayValue, isFocused]);
 
+  // Emit fader value to the server
   const emitValue = useCallback(
     (value: number) => {
       emit('fader_value', { deviceId: sliderGroupId, value: value, channelId: id });
@@ -55,6 +60,7 @@ const Fader: React.FC<SliderProps> = React.memo(({ id, sliderGroupId, name, numb
     [emit, sliderGroupId, id]
   );
 
+  // Always send the last value
   useEffect(() => {
     if (!timerRunning && cacheValueRef.current != null && cacheValueRef.current !== sendValueRef.current) {
       emitValue(faderValue);
@@ -67,6 +73,7 @@ const Fader: React.FC<SliderProps> = React.memo(({ id, sliderGroupId, name, numb
       setFaderValue(sliderGroupId, id, newValue);
       cacheValueRef.current = newValue;
 
+      // Send only at certain time intervals
       if (!timerRunning) {
         setTimerRunning(true);
         emitValue(newValue);
@@ -78,6 +85,7 @@ const Fader: React.FC<SliderProps> = React.memo(({ id, sliderGroupId, name, numb
     [setFaderValue, sliderGroupId, id, timerRunning, emitValue]
   );
 
+  // Check if the input value is a number
   const handleInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     if (value.length <= 5) {
@@ -85,6 +93,7 @@ const Fader: React.FC<SliderProps> = React.memo(({ id, sliderGroupId, name, numb
     }
   }, []);
 
+  // Check if the input is valid and set the fader value
   const handleInputConfirm = useCallback(() => {
     let numericValue = parseFloat(inputValue.toString().replace(',', '.'));
     if (!isNaN(numericValue)) {
@@ -95,17 +104,19 @@ const Fader: React.FC<SliderProps> = React.memo(({ id, sliderGroupId, name, numb
       const roundedDisplayValue = Math.round(numericValue);
       setInputValue(roundedDisplayValue);
     } else {
-      setInputValue(Math.round(scaledDisplayValue));
+      setInputValue(Math.round(scaledDisplayValue)); // Reset value if input is NaN
     }
     setIsFocused(false);
   }, [inputValue, scaledDisplayValue, setFaderValue, sliderGroupId, id, emitValue]);
 
+  // Confirm with ENTER
   const handleKeyDown = useCallback((event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      event.currentTarget.blur();
+      event.currentTarget.blur(); // Remove focus from the input
     }
   }, []);
 
+  // Scroll wheel and arrow keys support
   useEffect(() => {
     const updateFaderValue = (delta: number) => {
       const currentValue = faderValue;
@@ -117,7 +128,7 @@ const Fader: React.FC<SliderProps> = React.memo(({ id, sliderGroupId, name, numb
     const handleWheel = (event: WheelEvent) => {
       if (!isHovered) return;
       event.preventDefault();
-      const step = event.ctrlKey ? 10 : 1;
+      const step = event.ctrlKey ? 10 : 1; // If CTRL is pressed, increase/decrease by 10, otherwise by 1
       updateFaderValue(-Math.sign(event.deltaY) * step);
     };
 
@@ -139,13 +150,16 @@ const Fader: React.FC<SliderProps> = React.memo(({ id, sliderGroupId, name, numb
     };
   }, [isHovered, faderValue, setFaderValue, sliderGroupId, id, emitValue]);
 
+  // On value input focus
   const handleFocus = useCallback((event: React.FocusEvent<HTMLInputElement>) => {
     setIsFocused(true);
+    // Delay to the next tick
     setTimeout(() => {
-      event.target.select();
+      event.target.select(); // Select the input text
     }, 50);
   }, []);
 
+  // Background color of the lower half of the fader
   const backgroundColor = color || 'var(--mainColor)';
   const gradientStyle = {
     background: `linear-gradient(to right, ${backgroundColor} 0%, ${backgroundColor} ${scaledDisplayValue}%, rgba(40, 40, 40, 0.7) ${scaledDisplayValue}%, rgba(40, 40, 40, 0.7) 100%)`,
